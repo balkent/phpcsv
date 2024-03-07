7b10e77cd182d3549366d9ff8694569a70407db7
Quentin BALCEREK <quentin.balcerek@gmail.com>
2024-03-08 00:07:41 +0100

    feat(csv): data convert with json schema

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,50 +8,50 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(name: 'user')]
 class User
 {
-	#[ORM\Id]
+    #[ORM\Id]
     #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue]
-	private int $id;
+    private int $id;
 
-	#[ORM\Column(type: 'string')]
-	private string $name;
+    #[ORM\Column(type: 'string')]
+    private string $name;
 
-	#[ORM\Column(type: 'string')]
-	private string $firstname;
+    #[ORM\Column(type: 'string')]
+    private string $firstname;
 
-	public function getId(): int
-	{
-		return $this->id;
-	}
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
-	public function setId($id): self
-	{
-		$this->id = $id;
+    public function setId($id): self
+    {
+        $this->id = $id;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getName(): string
-	{
-		return $this->name;
-	}
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
-	public function setName(string $name): self
-	{
-		$this->name = $name;
+    public function setName(string $name): self
+    {
+        $this->name = $name;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getFirstname(): string
-	{
-		return $this->firstname;
-	}
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
 
-	public function setFirstname(string $firstname): self
-	{
-		$this->firstname = $firstname;
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
 
-		return $this;
-	}
+        return $this;
+    }
 }
